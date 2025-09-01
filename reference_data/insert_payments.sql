4dfb6eeb87a1b8e5b307b9c04e661a07538a542a
radifan9 <radif.aghnadiin@gmail.com>
2025-09-01 16:32:23 +0700

    feat: add SQL query to add payment method

---
+++ reference_data/insert_payments.sql
@@ -0,0 +1,10 @@
+insert into payments
+(method, img) values 
+('Google Pay', '/google_pay.png'),
+('Visa', '/visa.png'),
+('Gopay', '/gopay.png'),
+('Paypal', '/paypal.png'),
+('Dana', '/dana.png'),
+('BCA', '/bca.png'),
+('BRI', '/bri.png'),
+('OVO', '/ovo.png');
