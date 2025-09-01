d44ef7d2f2740f10a4dc85b5e164256ab5f2b1cf
radifan9 <radif.aghnadiin@gmail.com>
2025-09-01 15:21:25 +0700

    feat: add query for getting the sold seat for certain schedule

--- order/get_seat_sold.sql
+++ order/get_seat_sold.sql
@@ -1,6 +1,6 @@
 -- Used in order page where user need to choose a seat.
 -- Asumption : user already know the schedule (movie_id, city_id, time_id, cinemas_id, show_date)
--- Based on that schedule search the sold seat
+-- Based on that schedul, search the sold seat
 -- Logic
 -- Search all transaction with specified schedule_id
 select
@@ -10,6 +10,6 @@ select
 from
   transactions t
   join transactions_seats ts on ts.transactions_id = t.id
-  join seats_code sc on sc.id = ts.seats_id
+  join seat_codes sc on sc.id = ts.seats_id
 where
   t.schedule_id = 4;
